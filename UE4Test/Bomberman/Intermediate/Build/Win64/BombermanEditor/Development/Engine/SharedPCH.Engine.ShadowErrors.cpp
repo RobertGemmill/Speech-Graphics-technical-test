f849f68180e52c77de754a2eb2c5dfa77b9e5dee
Robert Gemmill <Robert@gemmill.org>
2021-10-29 16:42:49 +0100

    Set up empty projects
    Set up a new Empty UE4 projects
    Set up a blank VS project

---
+++ UE4Test/Bomberman/Intermediate/Build/Win64/BombermanEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29914
+#include "E:/Unreal/Speech-Graphics-technical-test/UE4Test/Bomberman/Intermediate/Build/Win64/BombermanEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ UE4Test/Bomberman/Intermediate/Build/Win64/UE4Editor/Inc/Bomberman/BombermanGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BOMBERMAN_BombermanGameModeBase_generated_h
+#error "BombermanGameModeBase.generated.h already included, missing '#pragma once' in BombermanGameModeBase.h"
+#endif
+#define BOMBERMAN_BombermanGameModeBase_generated_h
+
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_SPARSE_DATA
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_RPC_WRAPPERS
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABombermanGameModeBase(); \
+	friend struct Z_Construct_UClass_ABombermanGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABombermanGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Bomberman"), NO_API) \
+	DECLARE_SERIALIZER(ABombermanGameModeBase)
+
+
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesABombermanGameModeBase(); \
+	friend struct Z_Construct_UClass_ABombermanGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ABombermanGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Bomberman"), NO_API) \
+	DECLARE_SERIALIZER(ABombermanGameModeBase)
+
+
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABombermanGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABombermanGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABombermanGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABombermanGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABombermanGameModeBase(ABombermanGameModeBase&&); \
+	NO_API ABombermanGameModeBase(const ABombermanGameModeBase&); \
+public:
+
+
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABombermanGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABombermanGameModeBase(ABombermanGameModeBase&&); \
+	NO_API ABombermanGameModeBase(const ABombermanGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABombermanGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABombermanGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABombermanGameModeBase)
+
+
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_12_PROLOG
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_SPARSE_DATA \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_RPC_WRAPPERS \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_INCLASS \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_SPARSE_DATA \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Bomberman_Source_Bomberman_BombermanGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> BOMBERMAN_API UClass* StaticClass<class ABombermanGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Bomberman_Source_Bomberman_BombermanGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
